3a0f80b46bb3579a00bce38068d1b585f0e9443a
gsyoung2020 <gsyoung2019@gmail.com>
2021-06-21 18:38:54 -0400

    added more db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_221427) do
+ActiveRecord::Schema.define(version: 2021_06_21_223657) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2021_06_21_221427) do
     t.integer "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|

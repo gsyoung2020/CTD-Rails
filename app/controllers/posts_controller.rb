834e931bbb78c0cee5089dddd858895e2943e2bf
gsyoung2020 <gsyoung2019@gmail.com>
2021-06-28 21:58:08 -0400

    got the posts running

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,63 @@
 class PostsController < ApplicationController
-    
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+   
     def index
         @category=Category.find(params[:category_id])
         @posts = @category.posts
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def edit
+    end
+      
+    def create
+        @post = Post.new(post_params)
+    
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to action: "index", category_id: @post.category.id , notice: 'Post was successfully created.' }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+    
+    def update
+        respond_to do |format|
+          if @post.update(post_params)
+            format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+            format.json { render :show, status: :ok, location: @post}
+          else
+            format.html { render :edit }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def destroy
+        @post.destroy
+        respond_to do |format|
+          format.html { redirect_to categories_path, notice: 'Post was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_post
+        @post = Post.find(params[:id])
+      end
+    
+      # Only allow a list of trusted parameters through.
+      def post_params
+        params.require(:post).permit(:title, :body, :tag, :category_id, :user_id)
+      end
 end
